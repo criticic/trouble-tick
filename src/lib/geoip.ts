e6678f7244013c4d2a6f4845657e0164e1d2c547
Sagnik Mandal <acriticalcynic@outlook.com>
2025-01-28 17:41:28 +0530

    preload db

--- src/lib/geoip.ts
+++ src/lib/geoip.ts
@@ -1,21 +1,25 @@
+import fs from 'fs';
 import { GeoIpDbName, open } from 'geolite2-redist';
 import maxmind, { CityResponse } from 'maxmind';
 
-// const countryReader = await open(
-//     GeoIpDbName.Country,
-//     (path: string) => maxmind.open<CountryResponse>(path)
-// );
+const tempDir = '/tmp/geolite2-redist/dbs-tmp';
 
-const cityReader = await open(GeoIpDbName.City, (path: string) =>
-    maxmind.open<CityResponse>(path),
-);
+const cityReader = await open(GeoIpDbName.City, (dbPath: string) =>
+    maxmind.open<CityResponse>(dbPath), tempDir);
+
 
 export function getLocation(ip: string) {
+    // Ensure the directory exists
+    fs.mkdirSync(tempDir, { recursive: true });
+
     const lookup = cityReader.get(ip);
     if (!lookup) return null;
+
     console.log(lookup);
+
     const location = lookup.city?.names?.en
         ? lookup.city.names.en + ', ' + lookup.country?.names?.en
         : lookup.country?.names?.en;
+
     return location;
 }
